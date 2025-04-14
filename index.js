b1a3e0caf39dbfb6ebbfd5a09b595e9125be66ce
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2025-04-14 17:02:07 +0530

    created backend express server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import detenv from 'dotenv'
+import { connectDB } from './Database/db.js';
+
+detenv.config();
+
+const app = express()
+
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+}); 
+
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:${port}');
+
+    connectDB();
+});
